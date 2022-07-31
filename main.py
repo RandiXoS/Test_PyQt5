da067fe74d63bff171aeb4d2a5dbdd285d4bdb43
nFXIB0FF <astralprojectionbank@gmail.com>
2022-07-31 19:48:41 -0400

    July 31, 2022 7:48 PM

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import os
+import sys
 from PyQt5 import QtWidgets, QtCore, QtGui
+
+
+
+#Config App
+def Init():
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QWidget()
+    window.setWindowTitle("Hello World")
+    window.resize(300, 300)
+    window.show()
+    sys.exit(app.exec_())
+
+
 
 def main():
     print("Hello World!")
 
+#pera actigo gitdoc
 
 if __name__ == "__main__":
     main()
