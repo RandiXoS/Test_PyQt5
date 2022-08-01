e6dc630cd3927d90e6228354d9a98095bab094f5
nFXIB0FF <astralprojectionbank@gmail.com>
2022-07-31 20:01:47 -0400

    July 31, 2022 8:01 PM

--- practicas/Xeo/0_Test.py
+++ practicas/Xeo/0_Test.py
@@ -4,7 +4,6 @@ from PyQt5 import QtWidgets, QtCore, QtGui
 
 
 
-#Config App
 def Init():
     app = QtWidgets.QApplication(sys.argv)
     window = QtWidgets.QWidget()
@@ -17,21 +16,4 @@ def Init():
 
 def main():
     Init()
-    #print("Hello World!")
-
-
-
-"""
-Si dos carpetas, una tuya y una mia
-recien me acuerdo que se puede comentar así XD
-Jajajaajj entiendo XDD
-deja hago las carpetas
-"""
-"""
-Estaba pensando mas adelante hacer una plantilla para las practicas
-así no copiamos el codigo basico a cada rato. pero será después
-"""
-
-# if __name__ == "__main__":
-#     main()
-
+  
